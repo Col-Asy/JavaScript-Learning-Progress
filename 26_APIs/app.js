25aa0fd35df91adbd291b2fae25a4ade71f0f478
Ayush Barnwal <71561407+Col-Asy@users.noreply.github.com>
2024-06-24 19:42:00 +0530

    Express JS Completed

---
+++ 26_APIs/app.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}));   // this will make the body readable
+app.use(express.json());    // To make axios data readable
+
+let students = [
+    "Abhishek Pandey", "Archana", "Varda", "Ishan Gupta"
+];
+
+let teachers = [
+    "Kartik", "Monu", "Sabeel", "Mosina"
+];
+
+// IMPLEMENTING THE FOLLOWING APIs
+// 1. GET '/students': GET Details of all students
+app.get('/students', (req, res) => {
+    res.send(students);
+})
+
+// 2. POST '/student': Add one student to students[]
+app.post('/students', (req, res) => {
+    let { name } = req.body; // get the data from request body
+    students.push(name);
+    res.redirect('/students');
+})
+
+// 3. POST '/student/remove': Remove the student by name provided in queryParam
+app.post('/students/remove',(req,res)=>{
+    let {name} = req.body;
+    students = students.filter(s => s!==name);
+    res.redirect('/students');
+})
+
+// 4. GET 'teachers': GET details of all teachers
+app.get('/teachers', (req, res) => {
+    res.send(teachers);
+})
+
+// 5. POST '/teachers/update': Update the name of the teacher using body {oldName, newName}
+app.post("/teachers/update", (req, res) => {
+    let { oldName, newName } = req.body;
+    let indx = teachers.indexOf(oldName);
+    if (indx != -1) teachers[indx] = newName;
+
+    res.redirect('/teachers');
+})
+
+// For example: body : {oldName: "Kartik", newName: "Kartik Mathur"}
+// ["Kartik", "Monu", "Sabeel", "Mosina"]
+// ["Kartik Mathur", "Monu", "Sabeel", "Mosina"]
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:`+PORT);
+});
